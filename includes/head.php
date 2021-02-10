7a671b9854673f0236057d75d1d09658ad4ceb54
Giovanna Salgueiro <78795149+GiovannaSalgueiro@users.noreply.github.com>
2021-02-10 09:27:31 +0000

    Add files via upload

---
+++ includes/head.php
@@ -0,0 +1,30 @@
+<?php
+
+    echo "<meta charset='utf-8'>
+  <meta content='width=device-width, initial-scale=1.0' name='viewport'>
+
+  <meta content='' name='descriptison'>
+  <meta content='' name='keywords'>
+
+  <!-- Font awesome -->
+  <script src='https://kit.fontawesome.com/e8e2985ace.js' crossorigin='anonymous'></script>
+
+  <!-- Favicons -->
+  <link href='assets/img/favico.png' rel='icon'>
+  <link href='assets/img/apple-touch-icon.png' rel='apple-touch-icon'>
+
+  <!-- Google Fonts-->
+<link href='https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Satisfy' rel='stylesheet'>
+    
+
+  <!-- Vendor CSS Files -->
+  <link href='assets/vendor/bootstrap/css/bootstrap.min.css' rel='stylesheet'>
+  <link href='assets/vendor/icofont/icofont.min.css' rel='stylesheet'>
+  <link href='assets/vendor/boxicons/css/boxicons.min.css' rel='stylesheet'>
+  <link href='assets/vendor/owl.carousel/assets/owl.carousel.min.css' rel='stylesheet'>
+  <link href='assets/vendor/venobox/venobox.css' rel='stylesheet'>
+
+  <!-- Template Main CSS File -->
+  <link href='assets/css/style.css' rel='stylesheet'>"
+
+?>
